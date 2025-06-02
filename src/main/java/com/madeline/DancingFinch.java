1a5487f922b0c7c29e23d728b0b808b588d4f52e
madeline <mhinman0003@mymail.lausd.net>
2025-06-02 09:34:29 -0700

    update dancing finch

--- src/main/java/com/madeline/DancingFinch.java
+++ src/main/java/com/madeline/DancingFinch.java
@@ -12,7 +12,17 @@ public class DancingFinch {
 
         }
 
+        bird.setTail("all", 0, 100, 0);
+        bird.playNote(60,0.5);
+        bird.setTurn("L",360,50);
+        bird.setTurn("R", 180, 50);
 
+        bird.setMove("B", 30, 100);
+        bird.setTail(1,20,10,30);
+        bird.setTurn("L", 45, 50);
+        
+      
+      
         bird.stop();
         bird.disconnect();
     }
