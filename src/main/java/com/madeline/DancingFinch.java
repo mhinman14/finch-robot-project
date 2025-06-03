c482982e7081e63f2ab5092001e27eb6f09c9023
madeline <mhinman0003@mymail.lausd.net>
2025-06-03 09:14:07 -0700

    updated

--- src/main/java/com/madeline/DancingFinch.java
+++ src/main/java/com/madeline/DancingFinch.java
@@ -7,23 +7,30 @@ public class DancingFinch {
         Finch bird = new Finch();
 
         for (int i = 0; i < 4; i++) {
-        bird.setMove("F", 30, 100);
+        bird.setMove("F", 30, 100); //moves finch in square motion
         bird.setTurn("R", 90, 100);
 
         }
 
-        bird.setTail("all", 0, 100, 0);
+        bird.setTail("all", 0, 100, 0); //lights, sound and begins dance
         bird.playNote(60,0.5);
         bird.setTurn("L",360,50);
         bird.setTurn("R", 180, 50);
 
-        bird.setMove("B", 30, 100);
+        bird.setMove("B", 30, 100); //moves backwards and changes color 
         bird.setTail(1,20,10,30);
         bird.setTurn("L", 45, 50);
-        
+       
+       for (int i = 0; i < 3; i++){
+        bird.playNote(35, 2);
+        bird.playNote(65, 2);
+        bird.playNote(100, 2);
+        bird.setTurn("R", 45, 100);
+        bird.setTurn("L", 30, 100);
+       }
       
       
-        bird.stop();
+        bird.stop(); //stops program and disconnects
         bird.disconnect();
     }
     
