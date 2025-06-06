ebe3393868cceca4c20d9cdb89d505138e61a79f
madeline <mhinman0003@mymail.lausd.net>
2025-06-06 13:10:38 -0700

    final commit dancing finch

--- src/main/java/com/madeline/DancingFinch.java
+++ src/main/java/com/madeline/DancingFinch.java
@@ -21,14 +21,33 @@ public class DancingFinch {
         bird.setTail(1,20,10,30);
         bird.setTurn("L", 45, 50);
        
-       for (int i = 0; i < 3; i++){
-        bird.playNote(35, 2);
-        bird.playNote(65, 2);
-        bird.playNote(100, 2);
+        
+        bird.playNote(64, 0.5); //plays first few notes of Mary Had A Little Lamb
+        bird.pause(0.5);
+        bird.playNote(62, 0.5);
+        bird.pause(0.5);
+        bird.playNote(60, 0.5);
+        bird.pause(0.5);
+        bird.playNote(64, 0.5);
+        bird.pause(1);
+        bird.playNote(64, 0.5);
+        bird.pause(1);
+        bird.playNote(64, 0.5);
+        bird.pause(1);
+
+        
+
+
+       for (int i = 0; i < 3; i++){ //moves bird right and left three times from for loop
         bird.setTurn("R", 45, 100);
         bird.setTurn("L", 30, 100);
        }
-      
+
+        bird.setTail(1, 100, 50, 0);
+        bird.setTail(2, 50, 100, 50);
+        bird.setTail(3, 0, 50, 100);
+        bird.setTail(4, 50, 50, 50);
+
       
         bird.stop(); //stops program and disconnects
         bird.disconnect();
